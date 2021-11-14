29fcb325a0558b2790bed5e2622a165ad65ba27d
hatskier <suvorovalex1997@gmail.com>
2021-11-14 21:50:13 +0100

    added a comment about redstone values

--- contracts/Example.sol
+++ contracts/Example.sol
@@ -15,7 +15,7 @@ contract Example {
   // Example of a read-only pure function
   function isOverpriced() public pure returns(bool) {
     uint256 tslaPrice = 0; // let's fetch the real price using RedStone oracles
-    return tslaPrice > 1000 * 10 ** 8;
+    return tslaPrice > 1000 * 10 ** 8; // RedStone values has 8 decimals
   }
 
   // Example of a read-only view function, that fetches the saved price from state
